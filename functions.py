1e07f04e27e1d10ff908ad11b2409961b0e5df95
Uxue Garcia <uxue.garcia@alumni.mondragon.edu>
2024-11-15 16:40:03 +0100

    Creation of datasets for modelling

--- functions.py
+++ functions.py
@@ -1,4 +1,7 @@
 import pandas as pd
+import numpy as np
+import seaborn as sns
+import matplotlib.pyplot as plt
 
 def identify_column_types(df):
     # cat columns
@@ -8,3 +11,33 @@ def identify_column_types(df):
     numerical_columns = df.select_dtypes(include=['float64', 'int64']).columns.tolist()
     
     return categorical_columns, numerical_columns
+
+
+def convert_category(df, cols):
+    for col in cols:
+        if col in df.columns:
+            df[col] = df[col].astype('category')
+        else:
+            print(f" '{col}' does not exists")
+    return df
+
+
+def boxplot_outliers(data, numeric_columns, n_cols=3):
+    # Calcular el número de filas necesario
+    n_rows = int(np.ceil(len(numeric_columns) / n_cols))
+    
+    # Crear la figura y los ejes
+    fig, axes = plt.subplots(n_rows, n_cols, figsize=(n_cols * 5, n_rows * 3))
+    axes = axes.flatten()  # Aplanar la matriz de ejes para fácil acceso
+    
+    # Generar los boxplots en cada eje
+    for i, col in enumerate(numeric_columns):
+        sns.boxplot(data=data, x=col, ax=axes[i])
+        axes[i].set_title(f'Outliers {col}')
+    
+    # Eliminar ejes sin gráfico en caso de que sobren espacios en la cuadrícula
+    for j in range(i + 1, len(axes)):
+        fig.delaxes(axes[j])
+    
+    plt.tight_layout()  # Ajuste para que no se superpongan
+    plt.show()
